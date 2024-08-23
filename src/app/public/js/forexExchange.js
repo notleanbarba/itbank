d52b617d4eab46fc60701c26a0520fa9c5490d93
Leandro Barba <35031625+notleanbarba@users.noreply.github.com>
2024-08-22 23:04:50 -0300

    Catch forex API fetch

--- src/app/public/js/forexExchange.js
+++ src/app/public/js/forexExchange.js
@@ -25,38 +25,45 @@ document
   .replaceChildren(createCurrenciesList("USD"));
 
 const getCurrencies = async () => {
-  const tmp = await Promise.all(
-    currenciesList.map((currency) => {
-      return new Promise((res, rej) => {
-        fetch(`https://open.er-api.com/v6/latest/${currency}`)
-          .then((ans) => ans.json())
-          .then((ans) => {
-            res({ [currency]: ans.rates });
-          })
-          .catch((err) => rej(err));
-      });
-    }),
-  );
-  return tmp.reduce(
-    (obj, item) =>
-      Object.assign(obj, {
-        [Object.keys(item)[0]]: Object.values(item)[0],
+  try {
+    const tmp = await Promise.all(
+      currenciesList.map((currency) => {
+        return new Promise((res, rej) => {
+          fetch(`https://open.er-api.com/v6/latest/${currency}`)
+            .then((ans) => ans.json())
+            .then((ans) => {
+              res({ [currency]: ans.rates });
+            })
+            .catch((err) => rej(err));
+        });
       }),
-    {},
-  );
+    );
+    return tmp.reduce(
+      (obj, item) =>
+        Object.assign(obj, {
+          [Object.keys(item)[0]]: Object.values(item)[0],
+        }),
+      {},
+    );
+  } catch (e) {
+    throw new Error(e);
+  }
 };
 
-const rates = await getCurrencies();
+try {
+  const rates = await getCurrencies();
+  const calculateExchange = () => {
+    const input_currency = document.getElementById("from_currency").value;
+    const input_amount = document.getElementById("give_currency").value;
+    const output_currency = document.getElementById("to_currency").value;
+    const output_amount = rates[input_currency][output_currency] * input_amount;
 
-const calculateExchange = () => {
-  const input_currency = document.getElementById("from_currency").value;
-  const input_amount = document.getElementById("give_currency").value;
-  const output_currency = document.getElementById("to_currency").value;
-  const output_amount = rates[input_currency][output_currency] * input_amount;
+    document.getElementById("receive_currency").value = output_amount;
+  };
 
-  document.getElementById("receive_currency").value = output_amount;
-};
+  const exchangeCalculatorDOM = document.getElementById("exchange-calculator");
 
-const exchangeCalculatorDOM = document.getElementById("exchange-calculator");
-
-exchangeCalculatorDOM.addEventListener("input", calculateExchange);
+  exchangeCalculatorDOM.addEventListener("input", calculateExchange);
+} catch (e) {
+  console.log(e);
+}
