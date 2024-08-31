7f4f3eaaf3c824875d5a9fa02b578f0821d0537d
Leandro Barba <35031625+notleanbarba@users.noreply.github.com>
2024-08-30 22:29:35 -0300

    Website navigable with keyboard and accesible forms

--- src/app/assets/js/script.js
+++ src/app/assets/js/script.js
@@ -1,73 +1,56 @@
-document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
-document.getElementById("btn__registrarse").addEventListener("click", register);
-window.addEventListener("resize", anchoPage);
+document.getElementById('btn__iniciar-sesion').addEventListener('click', iniciarSesion);
+document.getElementById('btn__registrarse').addEventListener('click', register);
+window.addEventListener('resize', anchoPage);
 
-var formulario_login = document.querySelector(".formulario__login");
-var formulario_register = document.querySelector(".formulario__register");
-var contenedor_login_register = document.querySelector(
-  ".contenedor__login-register",
-);
-var caja__trasera_login = document.querySelector(".caja__trasera_login");
-var caja__trasera_register = document.querySelector(".caja__trasera_register");
+var formulario_login = document.querySelector('.formulario__login');
+var formulario_register = document.querySelector('.formulario__register');
+var contenedor_login_register = document.querySelector('.contenedor__login-register');
+var caja__trasera = document.querySelector('.caja__trasera');
 
 //Background
-const images = [
-  './public/login1.jpg',
-  './public/login2.jpg',
-  './public/login3.jpeg'
-];
+const images = ['./public/login1.jpg', './public/login2.jpg', './public/login3.jpeg'];
 
 let currentIndex = 0;
+
+var contenedorLogin = document.querySelector('.contenedor__todo');
+var contenedorRegister = document.querySelector('.contenedor__todo.reverse');
 
 //Las funciones cambian la opacidad del texto de registro y de login
 
 function anchoPage() {
   if (window.innerWidth > 850) {
-    caja__trasera_register.style.display = "block";
-    caja__trasera_login.style.display = "block";
+    caja__trasera.style.display = 'flex';
+    caja__trasera.style.display = 'flex';
   } else {
-    caja__trasera_register.style.display = "block";
-    caja__trasera_register.style.opacity = "1";
-    caja__trasera_login.style.display = "none";
-    formulario_login.style.display = "block";
-    contenedor_login_register.style.left = "0px";
-    formulario_register.style.display = "none";
+    caja__trasera.style.display = 'flex';
+    caja__trasera.style.opacity = '1';
+    caja__trasera.style.display = 'none';
+    formulario_login.style.display = 'flex';
+    contenedor_login_register.style.left = '0px';
+    formulario_register.style.display = 'none';
   }
 }
 
 anchoPage();
 
 function iniciarSesion() {
-  if (window.innerWidth > 850) {
-    formulario_login.style.display = "block";
-    contenedor_login_register.style.left = "10px";
-    formulario_register.style.display = "none";
-    caja__trasera_register.style.opacity = "1";
-    caja__trasera_login.style.opacity = "0";
-  } else {
-    formulario_login.style.display = "block";
-    contenedor_login_register.style.left = "0px";
-    formulario_register.style.display = "none";
-    caja__trasera_register.style.display = "block";
-    caja__trasera_login.style.display = "none";
-  }
+  contenedorRegister.classList.add('transition');
+  setTimeout(() => {
+    contenedorRegister.classList.remove('transition');
+    contenedorRegister.classList.add('hidden');
+    contenedorLogin.classList.remove('hidden');
+    document.getElementById('login-email').focus();
+  }, 500);
 }
 
 function register() {
-  if (window.innerWidth > 850) {
-    formulario_register.style.display = "block";
-    contenedor_login_register.style.left = "410px";
-    formulario_login.style.display = "none";
-    caja__trasera_register.style.opacity = "0";
-    caja__trasera_login.style.opacity = "1";
-  } else {
-    formulario_register.style.display = "block";
-    contenedor_login_register.style.left = "0px";
-    formulario_login.style.display = "none";
-    caja__trasera_register.style.display = "none";
-    caja__trasera_login.style.display = "block";
-    caja__trasera_login.style.opacity = "1";
-  }
+  contenedorLogin.classList.add('transition');
+  setTimeout(() => {
+    contenedorLogin.classList.remove('transition');
+    contenedorLogin.classList.add('hidden');
+    contenedorRegister.classList.remove('hidden');
+    document.getElementById('register-name').focus();
+  }, 500);
 }
 
 //Background
