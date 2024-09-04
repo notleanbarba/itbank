3828eacbbaccd7cbe9982b402716a303e3dd3041
Leandro Barba <35031625+notleanbarba@users.noreply.github.com>
2024-09-04 15:12:11 -0300

    Pages fix?

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://notleanbarba.github.io/itbank",
+  base: "itbank",
   plugins: [react()],
   css: {
     postcss: {
