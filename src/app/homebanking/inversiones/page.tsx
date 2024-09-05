7274a353b06ef84a7ba19e71268c0b009d480923
Matias LVM <matiasmaldonado.eth@gmail.com>
2024-09-04 22:33:50 -0300

    actualización vista de prestamos-pagos

--- src/app/homebanking/inversiones/page.tsx
+++ src/app/homebanking/inversiones/page.tsx
@@ -1,3 +1,21 @@
-export default function Inversiones() {
-  return <>Inversiones</>;
-}
+import "../../assets/styles/sass/responsive.scss";
+import "../../global.scss";
+import logoCompleto from '../../assets/images/logoCompleto.png';
+import logoITBA from '../../assets/images/logoITBA.png';
+
+
+export default function Pagos() {
+  return (
+    <>
+  <header>
+      <div className="logoCompleto">
+        <img src={logoCompleto} alt="Logo de Argentarius" width="90px" height="50px" />
+      </div>
+      <img src={logoITBA} alt="Logo de ITBA" width="90px" height="50px" />
+    </header>
+
+
+
+    </>
+  );
+};
