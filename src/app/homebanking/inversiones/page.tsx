66f546ed68cd0824f4835bb89a8d632ea6a6b88e
Pato-deve <patriciomartineztrabajo@gmail.com>
2024-09-05 17:50:07 -0300

    /inversiones Api Polygon

--- src/app/homebanking/inversiones/page.tsx
+++ src/app/homebanking/inversiones/page.tsx
@@ -3,6 +3,7 @@ import "../../assets/styles/sass/responsive.scss";
 import "../../global.scss";
 import Footer from "../footer.tsx"; 
 import Encabezado from '../header.tsx';
+import StockData from './component/exchangeApi.tsx';
 
 export default function Pagos() {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
@@ -15,10 +16,8 @@ export default function Pagos() {
     <div className="min-h-screen flex flex-col justify-between">
       <Encabezado toggleNavbar={toggleNavbar} toggleModal={() => {}} /> 
       
-      
-      
+      <StockData></StockData>
 
-      
       <Footer />
     </div>
   );
