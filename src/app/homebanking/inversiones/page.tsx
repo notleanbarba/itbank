ad48e6f1799e21c1ab24f6286e81fdd4930ff383
matiaslvm <matiasmaldonado.eth@gmail.com>
2024-09-05 08:23:21 -0300

    fix errors

--- src/app/homebanking/inversiones/page.tsx
+++ src/app/homebanking/inversiones/page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import "../../assets/styles/sass/responsive.scss";
 import "../../global.scss";
 import Footer from "../footer.tsx"; 
