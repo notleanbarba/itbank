1cdd7f82d1a7c7bdb047b82724ae650ca1f9dd72
Leandro Barba <35031625+notleanbarba@users.noreply.github.com>
2024-09-06 03:51:00 -0300

    Use Header and Footer in layout files

--- src/app/homebanking/footer.tsx
+++ src/app/homebanking/footer.tsx
@@ -1,62 +1,81 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faNewspaper,
+  faXmarkCircle,
+} from "@fortawesome/free-regular-svg-icons";
 
-
-const Footer: React.FC = ()=>{
-    return(
-        <>
-        <footer className="footer">
-      <div className="footer-content">
-        <div>
-          <h3>Nosotros</h3>
-          <a href="../about-us">Sobre nosotros</a>
-          <a href="../faq">FAQ</a>
-          <button type="button" className="modal-open" name="contact-modal">
-            Contacto
-          </button>
-          <dialog className="modal" id="contact-modal">
-            <div className="modal-dialog" role="dialog">
-              <button type="button" className="modal-close" tabIndex={-1}>
-                <i className="fa-solid fa-xmark"></i>
-              </button>
-              <h3>Información de contacto</h3>
-              <ul>
-                <li>Dirección:<span>Calle falsa 1234</span></li>
-                <li>Email:<span>itbank@corpo.com</span></li>
-                <li>Número:<span>+54 9 123456789</span></li>
-              </ul>
-            </div>
-          </dialog>
-        </div>
-        <div id="footer-contact">
-          <h3>Contacto</h3>
-          <address>
-            <ul>
-              <li>Dirección: <span>Calle falsa 1234</span></li>
-              <li>Email: <span>itbank@corpo.com</span></li>
-              <li>Número: <span>+54 9 123456789</span></li>
-            </ul>
-          </address>
-        </div>
-        <div className="newsletter">
-          <h3>Newsletter</h3>
-          <p>Suscribite a nuestro Newsletter para recibir ofertas especiales</p>
-          <form className="newsletter-input">
-            <input
-              type="email"
-              name="newsletter_email"
-              placeholder="Tu email"
-              aria-label="Email"
-              required
-            />
-            <button type="submit" className="submit-button" aria-label="enviar">
-              <i className="fa-solid fa-newspaper"></i>
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer-content">
+          <div>
+            <h3>Nosotros</h3>
+            <a href="../about-us">Sobre nosotros</a>
+            <a href="../faq">FAQ</a>
+            <button type="button" className="modal-open" name="contact-modal">
+              Contacto
             </button>
-          </form>
+            <dialog className="modal" id="contact-modal">
+              <div className="modal-dialog" role="dialog">
+                <button type="button" className="modal-close" tabIndex={-1}>
+                  <FontAwesomeIcon icon={faXmarkCircle} />
+                </button>
+                <h3>Información de contacto</h3>
+                <ul>
+                  <li>
+                    Dirección:<span>Calle falsa 1234</span>
+                  </li>
+                  <li>
+                    Email:<span>itbank@corpo.com</span>
+                  </li>
+                  <li>
+                    Número:<span>+54 9 123456789</span>
+                  </li>
+                </ul>
+              </div>
+            </dialog>
+          </div>
+          <div id="footer-contact">
+            <h3>Contacto</h3>
+            <address>
+              <ul>
+                <li>
+                  Dirección: <span>Calle falsa 1234</span>
+                </li>
+                <li>
+                  Email: <span>itbank@corpo.com</span>
+                </li>
+                <li>
+                  Número: <span>+54 9 123456789</span>
+                </li>
+              </ul>
+            </address>
+          </div>
+          <div className="newsletter">
+            <h3>Newsletter</h3>
+            <p>
+              Suscribite a nuestro Newsletter para recibir ofertas especiales
+            </p>
+            <form className="newsletter-input">
+              <input
+                type="email"
+                name="newsletter_email"
+                placeholder="Tu email"
+                aria-label="Email"
+                required
+              />
+              <button
+                type="submit"
+                className="submit-button"
+                aria-label="enviar"
+              >
+                <FontAwesomeIcon icon={faNewspaper} />
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </footer>
-        </>
-    )
+      </footer>
+    </>
+  );
 }
-
-export default Footer
--- src/app/homebanking/header.tsx
+++ src/app/homebanking/header.tsx
@@ -1,43 +1,132 @@
-import React, {useState, useRef, useEffect} from "react";
-import {NavLink} from "react-router-dom";
-import DropdownMenu from "./menu.tsx";
-import { HeaderProps } from "./page";
-import logoCompleto from '../assets/images/logoCompleto.png'
+import { useState, useRef, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
-const Usuario: React.FC = () => {
-  const [userProfile,setUserProfile] = useState(false);
+import logoCompleto from "../assets/images/logoCompleto.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
+
+function DropdownMenu() {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      className="text-black p-1 border border-black rounded-full"
+      ref={dropdownRef}
+    >
+      <i className="fa-solid fa-bars" />
+      <button
+        type="button"
+        onClick={() => {
+          setOpen(true);
+        }}
+        className="bg-white px-1 py-1 rounded-md focus:outline-none"
+      >
+        Menu
+      </button>
+      {open && (
+        <div className="absolute left-0 top-0 h-screen bg-white border border-gray-300 rounded-md shadow-lg z-10">
+          <button type="button" onClick={() => setOpen(false)}>
+            <FontAwesomeIcon icon={faXmarkCircle} className="w-5 h-5 m-4" />
+          </button>
+          <NavLink
+            to="/homebanking"
+            className="block px-4 py-4 text-black hover:bg-gray-100"
+          >
+            Inicio
+          </NavLink>
+          <NavLink
+            to="/homebanking/cuentas"
+            className="block px-4 py-4 hover:bg-gray-100"
+          >
+            Cuentas
+          </NavLink>
+          <NavLink
+            to="/homebanking/transferencias"
+            className="block px-4 py-4 hover:bg-gray-100"
+          >
+            Transferencias
+          </NavLink>
+          <NavLink
+            to="/homebanking/pagos"
+            className="block px-4 py-4 hover:bg-gray-100"
+          >
+            Pagos
+          </NavLink>
+          <NavLink
+            to="/homebanking/inversiones"
+            className="block px-4 py-4 hover:bg-gray-100"
+          >
+            Inversiones
+          </NavLink>
+          <NavLink
+            to="/homebanking/prestamos"
+            className="block px-4 py-4 hover:bg-gray-100"
+          >
+            Préstamos
+          </NavLink>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Usuario() {
+  const [userProfile, setUserProfile] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const toggleProfileMenu = () => {
     setUserProfile(!userProfile);
-  }
-
-  const handleClickOutside = (event: MouseEvent)=>{
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)){
-      setUserProfile(false);
-    }
-  }
+  };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setUserProfile(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
-  return(
+  return (
     <div className="relative" ref={dropdownRef}>
-      <button 
-        type="button" 
-        className="user-open" 
-        aria-label="Usuario" 
-        onClick={toggleProfileMenu}>
-        <i className="fa-solid fa-user fa-2xl"></i>
+      <button
+        type="button"
+        className="user-open"
+        aria-label="Usuario"
+        onClick={toggleProfileMenu}
+      >
+        <FontAwesomeIcon icon={faUser} />
+        <i className="fa-solid fa-user fa-2xl" />
       </button>
-      
+
       {userProfile && (
         <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg z-50">
-          <NavLink to='/'
+          <NavLink
+            to="/"
             className="block px-4 py-2 text-black hover:bg-gray-100 w-full text-left"
           >
             Cerrar Sesión
@@ -46,25 +135,22 @@ const Usuario: React.FC = () => {
       )}
     </div>
   );
-};
+}
 
-
-export const Encabezado: React.FC <HeaderProps> = () =>{
-    return (
-      <div className='flex justify-between border-b border-black items-center bg-white'>
-          <DropdownMenu/>
-          <div className="logoCompleto">
-            <img
-              src={logoCompleto}
-              alt="Logo de Argentarius"
-              className='w-[70px) h-[70px]'
-            />
-          </div>
-          <div className="border border-slate-950 rounded-full p-2">
-            <Usuario/>
-          </div>
+export default function Header() {
+  return (
+    <div className="flex justify-between border-b border-black items-center bg-white">
+      <DropdownMenu />
+      <div className="logoCompleto">
+        <img
+          src={logoCompleto}
+          alt="Logo de Argentarius"
+          className="w-[70px) h-[70px]"
+        />
       </div>
-    );
-  };
-
-export default Encabezado;
+      <div className="border border-slate-950 rounded-full p-2">
+        <Usuario />
+      </div>
+    </div>
+  );
+}
