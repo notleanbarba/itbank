571c9e76dfe2d5737e0a57c6d834f4f734f2fb82
Pato-deve <patriciomartineztrabajo@gmail.com>
2024-09-18 20:40:36 -0300

    Se arregla la carga de fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,13 @@ import type { ReactNode } from "react";
 export const metadata: Metadata = {
   title: "ITBANK homebanking",
   description:
-    "Banco Futuro ofrece cuentas flexibles, préstamos accesibles y herramientas de inversión innovadoras. Disfruta de banca en línea segura y atención personalizada para lograr tus objetivos financieros.",
+    "Banco Argentarius ofrece cuentas flexibles, préstamos accesibles y herramientas de inversión innovadoras. Disfruta de banca en línea segura y atención personalizada para lograr tus objetivos financieros.",
 };
 
 const roboto = Roboto_Mono({
   subsets: ["latin"],
   display: "swap",
+  preload: true,
 });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
