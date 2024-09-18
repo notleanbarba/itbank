d4d29baee2cab730504b1852339a638477a929e4
Pato-deve <patriciomartineztrabajo@gmail.com>
2024-09-18 01:09:21 -0300

    fix performance in Transferencias

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
 
 const roboto = Roboto_Mono({
   subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
