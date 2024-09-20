2f888d926fca44f02e46b2711d4bcb5c3b05822d
matias <o_maty@yahoo.com.ar>
2024-09-20 03:32:43 -0300

    arreglado en tarjetas
    cambiado el any por valores datostarjeta y cliente

--- src/app/homebanking/tarjetas/page.tsx
+++ src/app/homebanking/tarjetas/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { useState, useEffect } from "react";
 import Clientes from "@/app/data/cliente"; // Datos de los clientes
+import { Cliente, datoTarjeta } from "@/types"; // Asegúrate de importar los tipos adecuados
 import { Cartas } from "@components/credito"; // Importar el componente Cartas
 
 export default function Tarjeta() {
-  const [clienteAutenticado, setClienteAutenticado] = useState<any>(null);
+  const [clienteAutenticado, setClienteAutenticado] = useState<Cliente | null>(null);
 
   useEffect(() => {
     // Obtener el cliente ID de localStorage
     const storedClienteId = localStorage.getItem("clienteId");
 
     if (storedClienteId) {
-      const cliente = Clientes.find((cliente) => cliente.id === storedClienteId);
+      const cliente = Clientes.find((cliente) => cliente.id === storedClienteId) || null;
       setClienteAutenticado(cliente);
     }
   }, []);
@@ -23,9 +24,9 @@ export default function Tarjeta() {
 
   return (
     <div className="flex justify-center pt-10 min-h-screen bg-gray-100">
-    {clienteAutenticado.tarjeta.map((tarjeta: any) => (
-      <Cartas key={`tarjeta_${tarjeta.numeroTarjeta}`} tarjeta={tarjeta} />
-    ))}
-  </div>
+      {clienteAutenticado.tarjeta.map((tarjeta: datoTarjeta) => (
+        <Cartas key={`tarjeta_${tarjeta.numeroTarjeta}`} tarjeta={tarjeta} />
+      ))}
+    </div>
   );
 }
