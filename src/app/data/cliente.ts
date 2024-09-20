82602c331fc82a368158c0d539e1d015492ba71e
Pato-deve <patriciomartineztrabajo@gmail.com>
2024-09-20 16:21:03 -0300

    Repair error login and dinamic paths

--- src/app/data/cliente.ts
+++ src/app/data/cliente.ts
@@ -1,57 +1,79 @@
-import { Cliente } from "@types";
+import { Cliente, FacturaType } from "@types";
 
-const Clientes: Cliente[] = [
-  {
-    id: "132",
-    nombre: "Juan Perez",
-    email: "juan.perez@example.com",
-    password: "1234",
-    cuentas: [
-      { numeroCuenta: "123456", tipo: "Ahorros", saldo: 20000 },
-      { numeroCuenta: "342234", tipo: "Corriente", saldo: 5000 },
-    ],
-    tarjeta: [
-      {
-        numeroTarjeta: "5599 2838 4939 4884",
-        tipo: "Tarjeta de credito",
-        cvv: 123,
-      },
-    ],
-  },
-  {
-    id: "2",
-    nombre: "Maria Garcia",
-    email: "mariagarcia@example.com",
-    password: "1234",
-    cuentas: [
-      { numeroCuenta: "123453", tipo: "Ahorros", saldo: 30000 },
-      { numeroCuenta: "342223", tipo: "Corriente", saldo: 2000 },
-    ],
-    tarjeta: [
-      {
-        numeroTarjeta: "5599 2838 4239 4884",
-        tipo: "Tarjeta de credito",
-        cvv: 321,
-      },
-    ],
-  },
-  {
-    id: "3",
-    nombre: "Robert C. Martin",
-    email: "robert.martin@example.com",
-    password: "1234",
-    cuentas: [
-      { numeroCuenta: "234233", tipo: "Ahorros", saldo: 6000000 },
-      { numeroCuenta: "343234", tipo: "Corriente", saldo: 1000 },
-    ],
-    tarjeta: [
-      {
-        numeroTarjeta: "5599 2838 4349 4854",
-        tipo: "Tarjeta de credito",
-        cvv: 213,
-      },
-    ],
-  },
-];
+const obtenerFacturas = async (): Promise<FacturaType[]> => {
+  const response = await fetch("/facturas.json");
+  if (!response.ok) {
+    throw new Error("No cargo facturas.json");
+  }
+  const facturas = await response.json();
+  return facturas;
+};
 
-export default Clientes;
+export const obtenerClientes = async (): Promise<Cliente[]> => {
+  const facturas = await obtenerFacturas();
+
+  const Clientes: Cliente[] = [
+    {
+      id: "132",
+      nombre: "Juan Perez",
+      email: "juan.perez@example.com",
+      password: "1234",
+      cuentas: [
+        { numeroCuenta: "123456", tipo: "Ahorros", saldo: 20000 },
+        { numeroCuenta: "342234", tipo: "Corriente", saldo: 5000 },
+      ],
+      tarjeta: [
+        {
+          numeroTarjeta: "5599 2838 4939 4884",
+          tipo: "Tarjeta de credito",
+          cvv: 123,
+        },
+      ],
+      facturas: facturas.filter(
+        (facturas) => facturas.cliente === "Juan Perez",
+      ),
+    },
+    {
+      id: "2",
+      nombre: "Maria Garcia",
+      email: "mariagarcia@example.com",
+      password: "1234",
+      cuentas: [
+        { numeroCuenta: "123453", tipo: "Ahorros", saldo: 30000 },
+        { numeroCuenta: "342223", tipo: "Corriente", saldo: 2000 },
+      ],
+      tarjeta: [
+        {
+          numeroTarjeta: "5599 2838 4239 4884",
+          tipo: "Tarjeta de credito",
+          cvv: 321,
+        },
+      ],
+      facturas: facturas.filter(
+        (facturas) => facturas.cliente === "Maria Garcia",
+      ),
+    },
+    {
+      id: "3",
+      nombre: "Robert C. Martin",
+      email: "robert.martin@example.com",
+      password: "1234",
+      cuentas: [
+        { numeroCuenta: "234233", tipo: "Ahorros", saldo: 6000000 },
+        { numeroCuenta: "343234", tipo: "Corriente", saldo: 1000 },
+      ],
+      tarjeta: [
+        {
+          numeroTarjeta: "5599 2838 4349 4854",
+          tipo: "Tarjeta de credito",
+          cvv: 213,
+        },
+      ],
+      facturas: facturas.filter(
+        (facturas) => facturas.cliente === "Robert C. Martin",
+      ),
+    },
+  ];
+
+  return Clientes;
+};
--- src/app/homebanking/pagos/factura.tsx
+++ src/app/homebanking/pagos/factura.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FacturaType } from "@types";
+import { FacturaType, Cliente } from "@types";
 
 interface FacturaProps {
   id: string;
@@ -10,23 +10,28 @@ export default function Factura({ id }: FacturaProps) {
 
   useEffect(() => {
     if (!id) return;
-    async function fecthFactura() {
-      const res = await fetch(`/facturas.json`);
-      if (res.ok) {
-        const data = (await res.json()) as FacturaType[];
-        const facturaEncontrada = data.find(
-          (f) => f.id.toString() === id.toString(),
-        );
-        if (facturaEncontrada) {
-          setFactura(facturaEncontrada);
+
+    const storedCliente = localStorage.getItem("cliente");
+    if (storedCliente) {
+      const cliente: Cliente = JSON.parse(storedCliente);
+
+      async function fetchFactura() {
+        const res = await fetch(`/facturas.json`);
+        if (res.ok) {
+          const data = (await res.json()) as FacturaType[];
+
+          const facturaEncontrada = data.find(
+            (f) => f.id === id && f.cliente === cliente.nombre,
+          );
+
+          setFactura(facturaEncontrada || undefined);
         } else {
           setFactura(undefined);
         }
-      } else {
-        setFactura(undefined);
       }
+
+      fetchFactura();
     }
-    fecthFactura();
   }, [id]);
 
   if (factura === null) {
@@ -39,7 +44,7 @@ export default function Factura({ id }: FacturaProps) {
 
   return (
     <div className="m-1/12 bg-slate-200">
-      <h1 className="m-2">Detalles de la factura: </h1>
+      <h1 className="m-2">Detalles de la factura:</h1>
       <p className="m-2">Servicio: {factura.servicio}</p>
       <p className="m-2">Cliente: {factura.cliente}</p>
       <p className="m-2">Saldo a pagar: {factura.total}</p>
